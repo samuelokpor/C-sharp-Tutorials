afeba00e7080853383435ed5801906e5499632ad
songwl <songwl@20220401-01>
2023-08-22 15:47:52 +0800

    Add project files.

---
+++ Giraffe/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giraffe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string color, pluralNoun, celebrity;
+
+            Console.Write("Enter a color:");
+            color = Console.ReadLine();
+
+            Console.Write("Enter a plural noun:");
+            pluralNoun = Console.ReadLine();
+
+            Console.Write("Enter a celebrity:");
+            celebrity = Console.ReadLine();
+
+            Console.WriteLine("Roses are " + color);
+            Console.WriteLine(pluralNoun + " are blue");
+            Console.WriteLine("I love " + celebrity);
+
+
+            Console.ReadLine();
+        }
+    }
+}
